62ecf76f60a3d08468c8ddff7e76318c5750672f
Rebecca Chan <125217883+rebeccachan13@users.noreply.github.com>
2024-04-08 09:33:47 -0600

    Update Lab 6: Data Fetching

--- src/screens/AboutScreen.jsx
+++ src/screens/AboutScreen.jsx
@@ -3,11 +3,35 @@ import MainLayout from '../layouts/MainLayout';
 import {Text} from 'react-native';
 
 const AboutScreen = ({}) => {
+  const [message, setMessage] = React.useState('');
+
+  const apiCall = React.useCallback(async () => {
+    const response = await fetch(
+      'https://corporatebs-generator.sameerkumar.website/',
+      {
+        method: 'GET',
+      },
+    );
+    const json = await response.json();
+
+    //const phrase = json.phrase;
+    const {phrase} = json;
+
+    setMessage(phrase);
+  });
+
+  React.useEffect(() => {
+    apiCall().then(() => {
+      /*Called when the api call finishes*/
+    });
+  }, []);
+
   return (
     <MainLayout>
       <Text>Rebecca's Awesome To Do List App</Text>
       <Text>Written by: Rebecca Chan</Text>
-      <Text>Date: March 13, 2024</Text>
+      <Text>Date: April 8, 2024</Text>
+      <Text>{message}</Text>
     </MainLayout>
   );
 };
